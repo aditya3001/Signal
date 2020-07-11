7c30885a2bccb1768b1947273880e66027222582
HP <adityajainmathura@gmail.com>
2020-07-11 23:17:25 +0530

    Commit All

--- app/src/main/java/com/example/signal/MykiteTicker.java
+++ app/src/main/java/com/example/signal/MykiteTicker.java
@@ -1,26 +1,34 @@
 package com.example.signal;
 
+import android.content.Context;
+import android.database.Cursor;
+
 import com.zerodhatech.kiteconnect.KiteConnect;
 import com.zerodhatech.ticker.KiteTicker;
+import com.zerodhatech.ticker.OnConnect;
 import com.zerodhatech.ticker.OnTicks;
 
+import java.util.ArrayList;
+
 public class MykiteTicker extends KiteTicker {
-    private String[] InstrumentArray;
+    private String InstrumentArray;
     private float[] TargetArray;
     private float[] StopLossArray;
-
     public MykiteTicker(String accessToken, String apiKey) {
         super(accessToken, apiKey);
     }
+    MyDataBase myDataBase = new MyDataBase(null);
+    Cursor cursor = myDataBase.getData();
+
+
 
     @Override
-    public void setOnTickerArrivalListener(OnTicks onTickerArrivalListener) {
-        //TODO TICK PROCESSING
-        super.setOnTickerArrivalListener(onTickerArrivalListener);
+    public void setOnConnectedListener(OnConnect listener) {
+
     }
 
-    public void setInstrumentArray(String[] instrument){
-        this.InstrumentArray = instrument;
+    public void setInstrumentArray(){
+        this.InstrumentArray = cursor.getString(cursor.getColumnIndex("Stock_Name"));
     }
 
     public void setTargetArray(float[] targetArray){
@@ -30,5 +38,7 @@ public class MykiteTicker extends KiteTicker {
     public void getStopLossArray(float[] stopLossArray) {
         this.StopLossArray = stopLossArray;
     }
+
+
 }
 
