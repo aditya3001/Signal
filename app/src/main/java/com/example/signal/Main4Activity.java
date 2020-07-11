96f268164d4dc238dabbf774d48b161aa7b2c232
HP <adityajainmathura@gmail.com>
2020-07-11 13:10:28 +0530

    "app"

--- app/src/main/java/com/example/signal/Main4Activity.java
+++ app/src/main/java/com/example/signal/Main4Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ import com.zerodhatech.kiteconnect.KiteConnect;
 import com.zerodhatech.kiteconnect.kitehttp.exceptions.KiteException;
 import com.zerodhatech.models.Margin;
 import com.zerodhatech.models.Profile;
+import com.zerodhatech.models.User;
 
 import org.json.JSONException;
 
@@ -24,31 +26,58 @@ public class Main4Activity extends AppCompatActivity {
     Margin.Available Current_balance;
     TextView textview1,textview2;
     Switch switchbutton1;
+    String api_secret_key = new String("abc");  //need to keep it hidden
+    String public_token;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
         Intent intent = getIntent();
-        Mykiteconnect kiteSdk = new Mykiteconnect("tjcby5dbku38j51o");
+        KiteConnect kiteSdk = new KiteConnect("tjcby5dbku38j51o");
         request_token = intent.getParcelableExtra("request_token");
-//        try {
-//            Profile profile = kiteSdk.getProfile();
-//            Margin margin = kiteSdk.getMargins("equity");
-//            this.Profile_name = profile.userName;
-//            this.Current_balance = margin.available;
-//        } catch (KiteException e) {
-//            e.printStackTrace();
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+
+        try {
+            User user = kiteSdk.generateSession(request_token, api_secret_key);
+            public_token = user.publicToken;
+            SharedPreferences sharedPref = getParent().getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("access_token", user.accessToken);
+            editor.commit();
+        } catch (KiteException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SharedPreferences sharedPref = getParent().getPreferences(Context.MODE_PRIVATE);
+        String accessToken = sharedPref.getString("access_token", "abc");
+        kiteSdk.setAccessToken(accessToken);
+        kiteSdk.setPublicToken(public_token);
+
         textview1 = findViewById(R.id.textView1);
         textview2 = findViewById(R.id.textView2);
-        textview1.setText("Hi ");
-        textview1.setText(R.string.amount);
 
+
+        try {
+
+            Profile profile = kiteSdk.getProfile();
+            Margin margin = kiteSdk.getMargins("equity");
+            this.Profile_name = profile.userName;
+            this.Current_balance = margin.available;
+            textview1.setText("Hi "+ this.Profile_name);
+            textview1.setText("Woah! You have got: "+ this.Current_balance);
+
+        } catch (KiteException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
 
