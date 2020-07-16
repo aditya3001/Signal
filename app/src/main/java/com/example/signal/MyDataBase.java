09cb32d2dd9e88daec33594f3f6379e645fba9c4
litesh <litesharora999@gmail.com>
2020-07-16 14:01:24 +0530

    resolved issues

--- app/src/main/java/com/example/signal/MyDataBase.java
+++ app/src/main/java/com/example/signal/MyDataBase.java
@@ -19,6 +19,8 @@ public class MyDataBase extends SQLiteOpenHelper {
     public static final String Col_4 = "Stop_Loss";
     public static final String Col_5 = "Instrument_Token";
     public static final String Col_6 = "Time_Stamp";
+    public static final String Col_7 = "Hit";
+
 
     String[] Stock_name;
     Cursor res;
@@ -31,7 +33,7 @@ public class MyDataBase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE "+Tb_Name+" (ID INTEGER PRIMARY KEY AUTOINCREMENT,Stock_Name TEXT,Target float,Stop_Loss float,Instrument_Token long Default 0, Time_Stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP);");
+        db.execSQL("CREATE TABLE "+Tb_Name+" (ID INTEGER PRIMARY KEY AUTOINCREMENT,Stock_Name TEXT,Target float,Stop_Loss float,Instrument_Token long Default 0, Time_Stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, Hit TEXT);");
     }
 
     @Override
@@ -46,8 +48,22 @@ public class MyDataBase extends SQLiteOpenHelper {
         contentValues.put(Col_2,StckNme);
         contentValues.put(Col_3,Tgt);
         contentValues.put(Col_4,Stp_ls);
-
+        contentValues.put(Col_7,"Neutral");
         long result = db.insert(Tb_Name,null,contentValues);
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    public boolean updateData(long id,String StckNme, String Tgt,String Stp_ls, String hit ) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Col_2,StckNme);
+        contentValues.put(Col_3,Tgt);
+        contentValues.put(Col_4,Stp_ls);
+        contentValues.put(Col_7,hit);
+        long result = db.update(Tb_Name, contentValues, Col_1 + "=" + id, null);
         if(result == -1){
             return false;
         }else{
--- app/src/main/java/com/example/signal/SignalAdapter.java
+++ app/src/main/java/com/example/signal/SignalAdapter.java
@@ -2,13 +2,18 @@ package com.example.signal;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+//import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
 
 public class SignalAdapter extends RecyclerView.Adapter<SignalAdapter.SignalViewHolder> {
     private Cursor mCursor;
@@ -22,11 +27,13 @@ public class SignalAdapter extends RecyclerView.Adapter<SignalAdapter.SignalView
         public TextView companyName;
         public TextView target;
         public TextView stopLoss;
+        public TextView hit;
         public SignalViewHolder(@NonNull View itemView) {
             super(itemView);
             companyName = (TextView) itemView.findViewById(R.id.textview_name);
             target = (TextView)itemView.findViewById(R.id.textview_target);
             stopLoss = (TextView)itemView.findViewById(R.id.textview_stoploss);
+            hit = (TextView)itemView.findViewById(R.id.textview_hit);
 
         }
     }
@@ -39,6 +46,7 @@ public class SignalAdapter extends RecyclerView.Adapter<SignalAdapter.SignalView
         return new SignalViewHolder(view);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull SignalViewHolder holder, int position) {
         if(!mCursor.moveToPosition(position)){
@@ -47,11 +55,19 @@ public class SignalAdapter extends RecyclerView.Adapter<SignalAdapter.SignalView
         String company = mCursor.getString(mCursor.getColumnIndex(MyDataBase.Col_2));
         String target = mCursor.getString(mCursor.getColumnIndex(MyDataBase.Col_3));
         String stopLoss = mCursor.getString(mCursor.getColumnIndex(MyDataBase.Col_4));
+        String hit = mCursor.getString(mCursor.getColumnIndex(MyDataBase.Col_7));
         long id = mCursor.getLong(mCursor.getColumnIndex(MyDataBase.Col_1));
 
         holder.companyName.setText(company);
         holder.target.setText(target);
         holder.stopLoss.setText(stopLoss);
+
+        holder.hit.setText(hit);
+        if(hit.equalsIgnoreCase("Target"))
+            holder.hit.setTextColor(Color.parseColor("#008000"));
+        else if (hit.equalsIgnoreCase("Stop Loss"))
+            holder.hit.setTextColor(Color.parseColor("#FF0000"));
+
         holder.itemView.setTag(id);
     }
 
